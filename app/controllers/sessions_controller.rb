fd16bd581f8e52718939ceb29a74e334dbd37a91
Kazuwo <kazuwo95@gmail.com>
2018-03-14 11:51:34 +0900

    coment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,14 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
-      flash.now[:danger] = 'ログインに失敗しました'
+      flash[:danger] = 'ログインに失敗しました'
       render 'new'
     end
   end
 
   def destroy
     session.delete(:user_id)
-    flash[:notice] = 'ログアウトしました'
+    flash[:success] = 'ログアウトしました'
     redirect_to new_session_path
   end
 
