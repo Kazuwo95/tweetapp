c2e8e3ffd7342b5b07e33323436759a311683e18
Kazuwo <kazuwo95@gmail.com>
2018-04-01 19:46:07 +0900

    お問い合わせ機能改修

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,12 +92,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://agile-cove-84334.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-   user_name: ENV['SENDGRID_USERNAME'],
-   password: ENV['SENDGRID_PASSWORD'],
-   domain: "heroku.com",
-   address: "smtp.sendgrid.net",
-   port: 587,
-   authentication: :plain,
-   enable_starttls_auto: true
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => Settings.gmail[:user_name], #gmailアドレス
+    :password => Settings.gmail[:password], #gmailパスワード
+    :authentication => 'login'
   }
 end
