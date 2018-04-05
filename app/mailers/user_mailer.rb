9540774bdceb914e306734f861e2824994eb6c3f
Kazuwo <kazuwo95@gmail.com>
2018-04-05 17:23:33 +0900

    お問い合わせ機能調整

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,10 @@
 class UserMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
   def welcome_email(user)
-    @user = UserMailer
+    @user = user
     @url = 'https://agile-cove-84334.herokuapp.com/'
     mail(to: @user.email, subject: 'Monologueにご登録いただきありがとうございます！')
   end
+  
 end
