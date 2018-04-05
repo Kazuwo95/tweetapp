fdcbbc46c27fa32a9837dbba22826fb2f5165aae
Kazuwo <kazuwo95@gmail.com>
2018-04-05 17:31:15 +0900

    お問い合わせ機能調整

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to: "kazuwo95@gmail.com", subject: "お問合わせの確認メール"
+    mail to: "yyy.sol@ezweb.ne.jp", subject: "お問合わせの確認メール"
   end
 end
