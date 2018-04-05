5d3e6af295058409fdb7848ebdc10400d5ee821c
Kazuwo <kazuwo95@gmail.com>
2018-04-05 23:06:29 +0900

    お問い合わせ機能調整

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,16 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
+  config.action_mailer.default_url_options = { host: 'https://agile-cove-84334.herokuapp.com/' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => Settings.gmail[:user_name],
+    :password => Settings.gmail[:password],
+    :authentication => 'plain'
+  }
+
 end
