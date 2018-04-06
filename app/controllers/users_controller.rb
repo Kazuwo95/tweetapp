8d1c7103e0a3fc6b085d17c49215df85ad7683c3
Kazuwo <kazuwo95@gmail.com>
2018-04-06 18:06:41 +0900

    ユーザー登録時のメール送信の引数にミスがあったため、削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.welcome_email(@blog,@user).deliver
+      UserMailer.welcome_email(@user).deliver
       flash[:success] = "アカウントを作成しました！ログインしましょう！"
       redirect_to user_path(@user.id)
     else
